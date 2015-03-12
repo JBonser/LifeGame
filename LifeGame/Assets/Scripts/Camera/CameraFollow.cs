ec8ad088c9592122b7862294e3a492a7ecb83b38
Jordan Bonser <jordan_bonser@live.co.uk>
2015-03-12 19:32:14 +0000

    PlayerMovement and assets added

---
+++ LifeGame/Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform target;
+	public float smoothing = 5f;
+
+	public Vector3 offset = new Vector3( 0f, 0f, 0f );
+
+	void Start()
+	{
+		//offset = transform.position - target.position;
+	}
+
+	void FixedUpdate()
+	{
+		Vector3 targetCamPos = target.position + offset;
+
+		transform.position = Vector3.Lerp (target.position, targetCamPos, smoothing * Time.deltaTime );
+	}
+}
---
+++ LifeGame/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed = 6.0f;
+	public float turnSpeed = 6.0f;
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidBody;
+	int wallMask;
+	bool isJumping = false;
+	float camRayLength = 100.0f;
+
+	void Awake()
+	{
+		wallMask = LayerMask.GetMask ("WallMask");
+		anim = GetComponent<Animator> ();
+		playerRigidBody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+		bool jump = Input.GetButtonDown ("Jump");
+
+		Move (h, v);
+
+		Turning ();
+
+		Animating (h, v);
+
+	}
+
+	void Move( float h, float v )
+	{
+
+		movement.Set (h, 0.0f, 0.0f);
+
+		movement = movement.normalized * speed * Time.deltaTime;
+
+		playerRigidBody.MovePosition (transform.position + movement);
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit wallHit;
+
+		if (Physics.Raycast (camRay, out wallHit, camRayLength, wallMask))
+		{
+			Vector3 playerToMouse = wallHit.point - transform.position;
+
+			playerToMouse.z = 0.0f;
+			playerToMouse.y = 0.0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+			newRotation = Quaternion.RotateTowards( playerRigidBody.rotation, newRotation, turnSpeed * Time.deltaTime );
+			playerRigidBody.MoveRotation(newRotation);
+		}
+	}
+
+	void Animating(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking",walking );
+	}
+}
