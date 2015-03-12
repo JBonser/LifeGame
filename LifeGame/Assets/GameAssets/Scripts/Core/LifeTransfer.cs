fa5eaffa0e5f6e00ce16cdc1a9eeeffe09276fa0
Ashleigh Thomas <ashleighbcthomas@gmail.com>
2015-03-12 00:33:41 +0000

    Added CSharp assets and first life transfer script

---
+++ LifeGame/Assets/GameAssets/Scripts/Core/LifeTransfer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeTransfer : MonoBehaviour {
+
+	public float initialLife;
+	public float minLife;
+	public float maxLife;
+
+	private float currentLife;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void TransferLifeTo( LifeTransfer other )
+	{
+		currentLife -= 0.1f;
+		other.currentLife += 0.1f;
+	}	
+}
